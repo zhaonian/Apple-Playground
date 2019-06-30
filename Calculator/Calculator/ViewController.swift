26edb3f2b4745ca39ef5ac01d0de29f29934443c
Zhaonian Luan <lzn0830@gmail.com>
2019-06-30 16:38:02 -0700

    added Calculator Stack View Proj

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Zhaonian Luan on 6/30/19.
+//  Copyright © 2019 Zhaonian Luan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
