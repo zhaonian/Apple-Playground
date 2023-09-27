786795155611581566056ead057d211043f9b0fa
Keyu Gong <gky0121@keyus-mac-mini.lan>
2023-09-26 23:26:32 -0700

    News project setup

---
+++ News/News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Keyu Gong on 9/26/23.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
